00c5a35ae4024252bd910e3d02b26a831fa70435
moondabhay <abhaymoond1@gmail.com>
2025-05-30 14:48:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("your_render_url") ## change it with your host url
+    return web.json_response("https://txt-to-video-2-ywud.onrender.com") ## change it with your host url
 
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
